8bec1dc6b74244c3af2a7da6e939b73791f6fed8
Ygor Nascimento <ygor.nascimento@outlook.com>
2026-07-09 20:28:47 -0300

    Add base exceptions

---
+++ backend/src/main/java/br/tec/itlabs/backend/exception/RecursoNaoEncontradoException.java
@@ -0,0 +1,8 @@
+package br.tec.itlabs.backend.exception;
+
+public class RecursoNaoEncontradoException extends RuntimeException {
+
+    public RecursoNaoEncontradoException(String mensagem) {
+        super(mensagem);
+    }
+}
